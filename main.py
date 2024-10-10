7175916b3773f9106faa4c1b9a2fb84e91f81029
stephipie <stephi.pietschmann@gmail.com>
2024-10-10 14:08:50 +0200

    add main function

--- main.py
+++ main.py
@@ -30,7 +30,66 @@ def show_students():
     for student in students:
         print(student)  
 
-show_students()
 
-# add_student('stephi', 39, 'Techstarter')    
+# add_student('Abdullah', 25, 'Techstarter')
+
+
+# create update function to update students data
+def update_student(id,  name, age, course):
+    cursor.execute('''
+    UPDATE students SET name = ?, age = ?, course = ?
+    WHERE id = ? 
+''', (name, age, course, id))
+    conn.commit()
+    print(f"updated student with id {id}") 
+
+# create delete function to delete a student from students table
+def delete_student(id):
+    cursor.execute('''
+    DELETE FROM students WHERE id = ?
+    ''',(id,))
+    conn.commit()
+    print(f"Student with id {id} has been deleted.")    
+
+# define main function to get the user input
+# user can choose from create, read, update and delete function
+def main():
+    while True:
+        print("\n ------ Studentslist ------ ")
+        print("1. Add student")
+        print("2. Show student")
+        print("3. Update student")
+        print("4. Delete student")
+        print("5. End the programm")
+
+        choice = input("Please choose one option (1, 2, 3, 4 or 5): ")
+
+        if choice == "1":
+            print("Please enter the data o a new student: \n")
+            name = input("name: ")
+            age = input("age: ")
+            course = input("course: ")
+            add_student(name, age, course)
+        elif choice == "2":
+            show_students()
+        elif choice == "3":
+            print("Please update the data with id: ") 
+            id = input("id: ")
+            name = input("name: ")
+            age = input("age: ")
+            course = input("course: ")
+            update_student(id, name, age, course)
+        elif choice == "4":
+            print("Please enter the id of the student you want to delete: ")
+            id = input("id: ")
+            delete_student()
+        elif choice == "5":
+            print("Programm is ended. Goodbye.")
+            break
+        else:
+            print("Invalid input. Please choose between 1, 2, 3, 4 or 5")
+
+if __name__ == "__main__":
+    main()
+
 
